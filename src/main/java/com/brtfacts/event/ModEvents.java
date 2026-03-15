98e0282f29242c43116114eb9b2cfe3095d4f5f1
Long <ljr081031@outlook.com>
2026-03-15 07:07:16 +0000

    面向云代理会话的 VS Code 中的检查点

--- src/main/java/com/brtfacts/event/ModEvents.java
+++ src/main/java/com/brtfacts/event/ModEvents.java
@@ -1,12 +1,13 @@
 package com.brtfacts.event;
 
 import com.brtfacts.BrtFacts;
-import net.minecraft.world.entity.EquipmentSlot;
+import com.brtfacts.item.BunnySlippersItem;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import top.theillusivec4.curios.api.CuriosApi;
 
 @Mod.EventBusSubscriber(modid = BrtFacts.MODID)
 public class ModEvents {
@@ -15,10 +16,12 @@ public class ModEvents {
     public static void onLivingFall(LivingFallEvent event) {
         LivingEntity entity = event.getEntity();
         if (entity instanceof Player player) {
-            if (player.getItemBySlot(EquipmentSlot.FEET).getItem() == BrtFacts.BUNNY_SLIPPERS.get()) {
-                event.setDistance(0.0F);
-                event.setCanceled(true);
-            }
+            CuriosApi.getCuriosHelper()
+                    .findEquippedCurio(stack -> stack.getItem() instanceof BunnySlippersItem, player)
+                    .ifPresent(result -> {
+                        event.setDistance(0.0F);
+                        event.setCanceled(true);
+                    });
         }
     }
 }
--- src/main/java/com/brtfacts/item/SwiftScarfItem.java
+++ src/main/java/com/brtfacts/item/SwiftScarfItem.java
@@ -2,22 +2,25 @@ package com.brtfacts.item;
 
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterials;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
-public class SwiftScarfItem extends ArmorItem {
+public class SwiftScarfItem extends Item implements ICurioItem {
+    private static final int EFFECT_DURATION_TICKS = 220;
+
     public SwiftScarfItem() {
-        super(ArmorMaterials.LEATHER, Type.CHESTPLATE, new Properties().stacksTo(1));
+        super(new Properties().stacksTo(1));
     }
 
     @Override
-    @SuppressWarnings("removal")
-    public void onArmorTick(ItemStack stack, Level level, Player player) {
-        if (!level.isClientSide()) {
-            player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 220, 0, false, false, true));
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        LivingEntity entity = slotContext.entity();
+        if (!entity.level().isClientSide() && entity instanceof Player player) {
+            player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, EFFECT_DURATION_TICKS, 0, false, false, true));
         }
     }
 }
